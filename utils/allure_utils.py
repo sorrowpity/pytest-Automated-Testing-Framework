969c6710ca2fe175b7952cc7b69a82570169a5f7
Jenkins CI <jenkins@bot.local>
2026-08-12 14:05:33 +0800

    Allure report [#10]

--- utils/allure_utils.py
+++
@@ -1,8 +0,0 @@
-import allure
-
-def allure_init(case):
-    # 初始化allure报告
-    allure.dynamic.feature(case["feature"])
-    allure.dynamic.story(case["story"])
-    # allure.dynamic.title(case["title"])
-    allure.dynamic.title(f"ID: {case["id"]}--{case['title']}")
